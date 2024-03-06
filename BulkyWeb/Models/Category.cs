b8e1c42662367a3bd4901ee3bb73aa1ae0f590a0
z.zora <lily03316@yahoo.com.tw>
2024-03-06 22:26:06 +0800

    [DB] Create database and category table

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        // prop tab
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
