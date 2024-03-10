7d950e623f88701a58434ff7d9e07d9f207ba94b
z.zora <lily03316@yahoo.com.tw>
2024-03-11 00:38:47 +0800

    Revert "[add] Category CRUD function"
    This reverts commit e696462ad01272b22091d1886e644c5f211ccf89.

--- BulkyWeb/Migrations/ApplicationDbContextModelSnapshot.cs
+++ BulkyWeb/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -39,26 +39,6 @@ namespace BulkyWeb.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("Categories");
-
-                    b.HasData(
-                        new
-                        {
-                            Id = 1,
-                            DisplayOrder = 1,
-                            Name = "Action"
-                        },
-                        new
-                        {
-                            Id = 2,
-                            DisplayOrder = 2,
-                            Name = "SciFi"
-                        },
-                        new
-                        {
-                            Id = 3,
-                            DisplayOrder = 3,
-                            Name = "History"
-                        });
                 });
 #pragma warning restore 612, 618
         }
